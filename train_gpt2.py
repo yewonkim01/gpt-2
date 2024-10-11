6a4eb6308d05b5f1e0e66030e273d0eec684a14f
yewonkim <kyw9791@naver.com>
2024-10-11 16:55:44 +0000

    30

--- train_gpt2.py
+++ train_gpt2.py
@@ -2,11 +2,41 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 from dataclasses import dataclass
+import math
 
 class CausalSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0
+        self.c_attn = nn.Linear(config.n_embd, 3* config.n_embd)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        # why do we need this?
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)) \
+                             .view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size()
+        # isn't it C == n_embd ?????
+        qkv = self.c_attn(x) # (B, T, 3 * n_embd)
+        q, k, v = qkv.split(self.n_embd, dim=2) # each of (B, T, n_embd)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, n_head, T, head_size1)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1))) # (B, n_head, T, T)
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf')) # why ???
+        att = F.softmax(att, dim = -1) # Each token's score about all tokens
+        y = att @ v
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return y
+
+
+
+
+
 
 
 
@@ -44,7 +74,7 @@ class Block(nn.Module):
 
 @dataclass
 class GPTConfig:
-    block_size: int = 256 #??? block size ??
+    block_size: int = 256 #??? block size ?? number of tokens?
     vocab_size: int = 65
     n_layer: int = 6
     n_head: int = 6
